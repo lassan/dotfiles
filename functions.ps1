1036e83814aec5351fab412f9485c8caddcb4856
Hassan <hassanmunir@live.com>
2021-06-08 16:45:06 +0100

    Add Open-Location function

--- functions.ps1
+++ functions.ps1
@@ -1,13 +1,22 @@
 function Copy-CurrentLocation {
+    [alias("ccl")]
     param (       
         [switch]$BackSlash = $false
     )
 
     $path = (Get-Location).Path 
     
-    if (!$BaclSlash) { $path = $path -replace "\\", "/" }
+    if (!$BackSlash) { $path = $path -replace "\\", "/" }
 
     Set-Clipboard $path
     
     $path
+}
+
+function Open-Location {
+    [alias("explore")]
+    param([string] $path)
+
+    if ($path) { explorer.exe $path }
+    else { explorer.exe . }
 }
