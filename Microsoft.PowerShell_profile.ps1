be4be2ea2ce6f434bf2138bffa6fc2802c4ecbcd
Hassan <hassanmunir@live.com>
2021-07-15 13:24:44 +0100

    Add PSReadLine

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,26 @@
-if (!(Get-Module -ListAvailable -Name "oh-my-posh")) {
-    Install-Module oh-my-posh
-}
-if(!(Get-Module -ListAvailable -Name "posh-git")) {
-    Install-Module posh-git
-}
+LoadModule("posh-git")
 
+# on-my-posh
+LoadModule("oh-my-posh")
+Import-Module posh-git
 $env:POSH_GIT_ENABLED = $true
-
 Set-PoshPrompt avit
+
+# PSReadLine
+LoadModule("PSReadLine")
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView -MaximumHistoryCount 4
+Set-PSReadLineOption -EditMode Windows
 
 Push-Location (Split-Path $PROFILE)
 "functions" | Where-Object { Test-Path "$_.ps1" } | ForEach-Object -Process { Invoke-Expression ". .\$_.ps1" }
 Pop-Location
+
+function LoadModule {
+    param([string] $module)
+
+    if (!(Get-Module -ListAvailable -Name $module)) {
+        Install-Module $module
+    }
+}
